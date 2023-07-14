6430cd8e4c798377d30b48a7b2525b6082a1c8f2
wesley180320 <wesleyestevao333@gmail.com>
2023-07-13 21:33:56 -0400

    imagens e servelts part1

--- src/main/java/controller/controller.java
+++ src/main/java/controller/controller.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class controller
  */
-@WebServlet("/controller")
+@WebServlet(urlPatterns = {"/controller","/main"})
 public class controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
